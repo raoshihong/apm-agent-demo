1f797d65a3617742d0713a11534695061f5325bd
honey.rao <honey.rao@wolaidai.com>
2018-11-03 11:41:52 +0800

    java探针中agentmain学习

---
+++ src/main/java/com/rao/study/apm/agent/demo/agent_main/TestMainInJar.java
@@ -0,0 +1,13 @@
+package com.rao.study.apm.agent.demo.agent_main;
+
+public class TestMainInJar {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(new TransClass().getNumber());
+        while (true) {
+            Thread.sleep(2000);
+            int number = new TransClass().getNumber();
+            System.out.println(number);
+        }
+    }
+
+}
---
+++ src/main/java/com/rao/study/apm/agent/demo/agent_main/TransClass.java
@@ -0,0 +1,7 @@
+package com.rao.study.apm.agent.demo.agent_main;
+
+public class TransClass {
+    public int getNumber() {
+        return 0;
+    }
+}
